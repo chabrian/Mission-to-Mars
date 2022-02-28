a25511365fd2c9a33beee1e1b53072c4a3ca9b0b
briancha <briancha98@gmail.com>
2022-02-27 21:34:41 -0500

    updated scraping.py and index.html for Challenge

--- scraping.py
+++ scraping.py
@@ -14,13 +14,17 @@ def scrape_all():
     #store news output into variables
     news_title, news_paragraph = mars_news(browser)
 
+    #store hemisphere images into variable
+    hemisphere_images = hemisphere_data(browser)
+
     # Run all scraping functions and store results in dictionary
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()}
+        "last_modified": dt.datetime.now(),
+        "hemi_images": hemisphere_images}
 
     browser.quit()
     return data
@@ -89,9 +93,41 @@ def mars_facts():
 
     return df.to_html()
 
+def hemisphere_data(browser):
+    
+    # Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # function to extract image url and title
+    def findImage():
+        img_dict = {}
+        html = browser.html
+        image_soup = soup(html, 'html.parser')
+        img_url_rel = image_soup.find_all('a')
+        full_url = url + img_url_rel[3].get('href')
+        img_title = image_soup.find('h2').text
+        img_dict['img_url']=full_url
+        img_dict['title']=img_title
+        return img_dict
+
+    # Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # loop through image websites and append dictionary to list
+    for x in range(0,4):
+        full_image_elem = browser.find_by_tag('h3')[x]
+        full_image_elem.click()
+        hemisphere_image_urls.append(findImage())
+        browser.visit(url)
+
+    return hemisphere_image_urls
+
+
 if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
+
 
 
 
